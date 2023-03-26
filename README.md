36f4783610c18e1442b9ce4fd8fed3a8ee0c678c
Alfredo Sánchez Alberca <asalber@gmail.com>
2023-03-26 23:14:20 +0200

    Added DESeq2 analysis.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# rna-seq
+# RNA-Seq analysis
+
 Analysis of RNA-Seq for identifying gene differential expression.
+
+## Author 
+Alfredo Sánchez Alberca (asalber@ceu.es)
---
+++ preprocesing.R
@@ -0,0 +1,47 @@
+# Data preprocesing 
+# Alfredo Sánchez Alberca (asalber@ceu.es)
+
+# Libraries
+library(tidyverse)
+library(edgeR)
+
+# Data loading
+df_reads <- read_csv("data/counts.csv")
+
+# Rename individuals
+df_groups <- read_csv("data/groups.csv") %>%
+    mutate(Group = as.factor(Group))
+new_names <- df_groups %>%
+    select("Id", "Name") %>%
+    deframe()
+df_reads <- rename_with(df_reads, ~ df_groups$Id, all_of(df_groups$Name))
+
+# Create count matrix
+matrix_reads <- as.data.frame(select(df_reads, -CLUSTER))
+rownames(matrix_reads) <- df_reads$CLUSTER
+
+# Add experimental group
+df_reads_longer <- df_reads %>%
+    pivot_longer(cols = C1:DG2T7, names_to = "Id", values_to = "Count") %>%
+    left_join(df_groups, by = "Id")
+
+# reads loading
+reads <- DGEList(counts = matrix_reads, group = df_groups$Group)
+
+# Filtering genes with more than 5 reads in at least 3 individuals
+# keep <- rowSums(cpm(reads)> 0.5) >= 7
+# table(keep)
+
+# Automatic filtering
+keep <- filterByExpr(reads)
+reads <- reads[keep, , keep.lib.sizes=F]
+
+# Filter also the longer data frame
+df_reads_longer <- df_reads_longer %>%
+    filter(CLUSTER %in% names(keep[keep==T]))
+
+# Reads counts per million of reads
+reads_cpm <- cpm(reads)
+reads_cpm_longer <- as.data.frame(reads_cpm) %>%
+    pivot_longer(cols = C1:DG2T7, names_to = "Id", values_to = "CPM") %>%
+    left_join(df_groups, by = "Id")
